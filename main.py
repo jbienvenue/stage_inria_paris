8ada5e03fb561e7611cbaf895a3baa1001d32035
Joachim Bienvenüe <pjoi@passoire.fr>
2025-06-18 16:51:39 +0200

    better variable names and graphics, don't print everythings

--- main.py
+++ main.py
@@ -10,22 +10,24 @@ async def go_abandon(pos, Flast, correct, start):
     query_override.trajectory_complete = moteus.multiplex.INT8
     count = 2
     last = Flast
-    L = []
+    positions_engine = []
+    positions_stop = []
     await c.set_stop()
     cur = (await c.query()).values[1]
+    positions_stop.append(cur)
     while not correct(cur, start):
         cur = (await c.query()).values[1]
+        positions_stop.append(cur)
     last = cur
     while True:
         res = await c.set_position(position=pos, maximum_torque=0.025, accel_limit=9.8, query=True)
-        print(res)
         count = max(count-1, 0)
         cur = res.values[1]
-        L.append(cur)
+        positions_engine.append(cur)
         if not correct(cur, last) and correct(cur, start+(start-Flast)):
-            return cur, res, L
+            return cur, res, positions_engine, positions_stop
         if pos-eps < cur < pos+eps:
-            return cur, res, L
+            return cur, res, positions_engine, positions_stop
         last = cur
         await asyncio.sleep(0.02)
 
@@ -37,24 +39,26 @@ async def main():
     goalR = start+0.5
     goalL = start-0.5
     last = start
-    M = []
+    position_groups = []
     while True:
-        last, final, L = await go_abandon(goalR, last, lambda a,b:a>b, start)
-        print(final, goalR)
-        M.append(L)
+        last, final, positions_engine, positions_stop = await go_abandon(goalR, last, lambda a,b:a>b, start)
+        #print(final, goalR)
+        position_groups.append(positions_stop)
+        position_groups.append(positions_engine)
         if goalR-eps < last < goalR+eps:break
-        last, final, L = await go_abandon(goalL, last, lambda a,b:a<b, start)
-        print(final, goalL)
-        M.append(L)
+        last, final, positions_engine, positions_stop = await go_abandon(goalL, last, lambda a,b:a<b, start)
+        #print(final, goalL)
+        position_groups.append(positions_stop)
+        position_groups.append(positions_engine)
         if goalL-eps < last < goalL+eps:break
     await stop()
     plt.ion()
     start = 0
-    for n, data in enumerate(M):
-        plt.plot(range(start, start+len(data)), data, c='red' if n%2 else 'green')
+    for n, data in enumerate(position_groups):
+        plt.plot(range(start, start+len(data)), data, c=['green', 'red', 'green', 'orange'][n%4])
         start += len(data)
     plt.show(block=True)
-    print(M)
+    #print(position_groups)
     
 
 try:
