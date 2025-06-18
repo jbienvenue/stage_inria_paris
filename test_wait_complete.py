10649bfadc4d17d2e54623b0d2f3ce0576823731
Joachim Bienvenüe <pjoi@passoire.fr>
2025-06-18 14:28:16 +0200

    bit different, take in account the position at the start to turn of a quarter of a turn

--- test_wait_complete.py
+++ test_wait_complete.py
@@ -26,24 +26,24 @@ import moteus
 c = moteus.Controller(id=6)
 async def stop_all():
     await c.set_stop()
+
 async def main():
 
     # Clear any outstanding faults.
-    await c.set_stop()
-    try:
-        while True:
-            # This will periodically command and poll the controller until
-            # the target position achieves the commanded value.
-            result = await c.set_position_wait_complete(
-                position=2, accel_limit=2.0)
-            print(result)
-
-            # Then go back to zero, and eventually try again.
-            result = await c.set_position_wait_complete(
-                position=0.0, accel_limit=2.0)
-            print(result)
-    except KeyboardInterrupt:
-        await c.set_stop()
+    start = await c.set_stop(query=True)
+    null = start.values[1]
+    while True:
+        # This will periodically command and poll the controller until
+        # the target position achieves the commanded value.
+        result = await c.set_position_wait_complete(
+            position=null+0.25, accel_limit=2.0)
+        print(result)
+        print(result.values[1])
+        # Then go back to zero, and eventually try again.
+        result = await c.set_position_wait_complete(
+            position=null-0.25, accel_limit=2.0)
+        print(result)
+        print(result.values[1])
 
 
 if __name__ == '__main__':
